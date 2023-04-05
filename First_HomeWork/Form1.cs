08cabb293ec93e50fc1af64b82a035c74944e636
Daria <dashakukuruza86@gmail.com>
2023-04-05 17:58:19 +0300

    Add project files.

---
+++ First_HomeWork/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace First_HomeWork
+{
+    public partial class Form1 : Form
+    {
+        public static int MessageBoxCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBoxCount++;
+            string Text1 = "Меня зовут Кукуруза Дарья Романовна";
+            MessageBox.Show(Text1, "Резюме", MessageBoxButtons.OK);
+            MessageBoxCount++;
+            string Text2 = "Я являюсь начинающим программистом, владею языками программирования С++, С#, JS и имею опыт в создании приложений на Unity. " +
+                "Моя цель - получение опыта работы и постоянное совершенствование своих навыков в области программирования.";
+            MessageBox.Show(Text2, "Навыки", MessageBoxButtons.OK);
+            MessageBoxCount++;
+            string Text3 = "Я имею навыки работы с английским языком на уровне B1 и владею немецким языком на уровне A2, " +
+                "что позволяет мне эффективно общаться с иностранными коллегами и клиентами.";
+            MessageBox.Show(Text3, "Языки", MessageBoxButtons.OK);
+            MessageBoxCount++;
+            string Text4 = "Среднее образование (11 классов), школа №4\nНезаконченное высшее образование по специальности \"Тележурналистика\", Киевский Национальный Университет Культуры и Искусств.\nКурс по программированию на втором курсе в \"ШАГ\".";
+            MessageBox.Show(Text4, "Образование", MessageBoxButtons.OK);
+
+            string ResultText = Text1 + Text2 + Text3 + Text4;
+            double ResultCount = ResultText.Length /MessageBoxCount;
+            MessageBox.Show("Среднее число символов", $"{ResultCount}");
+        }
+    }
+}
---
+++ Guess_the_number/Form1.cs
@@ -0,0 +1,61 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Guess_the_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = "";
+            bool validInput = false;
+            int iterator = 0;
+            while (!validInput)
+            {
+                input = Microsoft.VisualBasic.Interaction.InputBox("Введите число:", "Запрос числа");
+                int num;
+                if (int.TryParse(input, out num))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    MessageBox.Show("Вы ввели некорректное значение");
+                }
+            }
+            int min = 0;
+            int max = 2000;
+            int guess = 0;
+            while (min <= max)
+            {
+                iterator++;
+                guess = (min + max) / 2;
+                DialogResult rez = MessageBox.Show($"Ваше число больше {guess}?", "Угадайка", MessageBoxButtons.YesNo);
+                if (rez == DialogResult.Yes)
+                {
+                    min = guess + 1;
+                }
+                else
+                {
+                    max = guess - 1;
+                }
+            }
+            MessageBox.Show($"Мы угадали ваше число это - {guess + 1}\nНам понадобалось {iterator} попыток", "Угадайка");
+        }
+    }
+}
